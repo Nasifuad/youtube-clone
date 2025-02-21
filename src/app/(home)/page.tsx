2c907a605a7a748b00d9f28313bdb1eba9114adb
Nasifuad <nasifuad007@gmail.com>
2025-02-21 15:26:18 +0600

    removed package.lock

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <></>;
+  return <div className="h-screen w-full bg-slate-800"></div>;
 };
 
 export default Page;
--- src/modules/home/ui/components/home-navbar/home-navbar.tsx
+++ src/modules/home/ui/components/home-navbar/home-navbar.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import SearchInput from "./search-input";
+import AuthButton from "@/modules/auth/ui/components/auth-button";
 
 const HomeNavbar = () => {
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+      <nav className="container mx-auto fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
         <div className="flex items-center gap-4 w-full">
           {/* menu and logo */}
           <div className="flex items-center flex-shrink-0">
