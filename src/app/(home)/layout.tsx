102f98ff9aec16f009be7d5d4a56912825b37f5a
Nasifuad <nasifuad007@gmail.com>
2025-02-21 15:53:41 +0600

    ww

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
+import { HomeLayout } from "../../modules/home/ui/layouts/home-layout";
 
 interface LayoutPros {
   children: React.ReactNode;
